3976bf059e03c32d975b53c3e2a428ab85b1654c
Jonas Harju <jonas.harju@gmail.com>
2025-05-16 00:27:25 +0200

    sadf

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import SpeechComponent from "@/app/components/SpeechComponent";
+import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
   return (
     <main>
       <h1>Minimal Speech to Text</h1>
       <SpeechComponent />
+      <Button>Click me</Button>
     </main>
   );
 }
